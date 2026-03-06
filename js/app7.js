4554dd0a6956065798c0b6bb6522762a4f15151e
Gustavo Leiva <leiva.gustavo@hotmail.com.ar>
2026-03-06 13:04:50 -0300

    mejora en calendario

--- js/app7.js
+++ js/app7.js
@@ -1,33 +1,15 @@
-async function obtenerReservas() {
-  const url = "https://calendar.google.com/calendar/ical/reservas.lagauchita%40gmail.com/public/basic.ics";
+document.addEventListener("DOMContentLoaded", () => {
 
-  const respuesta = await fetch(url);
-  const texto = await respuesta.text();
+flatpickr("#fechaEntrada",{
+  locale:"es",
+  dateFormat:"d/m/Y",
+  minDate:"today"
+});
 
-  const fechasOcupadas = [];
-
-  const eventos = texto.split("BEGIN:VEVENT");
-
-  eventos.forEach(evento => {
-    const inicio = evento.match(/DTSTART:(\d+)/);
-    const fin = evento.match(/DTEND:(\d+)/);
-
-    if (inicio && fin) {
-      fechasOcupadas.push({
-        from: inicio[1],
-        to: fin[1]
-      });
-    }
-  });
-
-  return fechasOcupadas;
-}
-
-obtenerReservas().then(fechas => {
-
-  flatpickr("#fechaEntrada", {
-    dateFormat: "d/m/Y",
-    disable: fechas
-  });
+flatpickr("#fechaSalida",{
+  locale:"es",
+  dateFormat:"d/m/Y",
+  minDate:"today"
+});
 
 });
